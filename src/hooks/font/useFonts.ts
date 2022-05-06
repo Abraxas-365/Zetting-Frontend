38a60f363594fecdf3f59491f37d950eeee473c9
Luis Fernando Miranda <abraxas@abraxas-3.local>
2022-05-06 18:37:26 -0500

    organizing

--- src/hooks/font/useFonts.ts
+++
@@ -1,9 +0,0 @@
-import * as Font from 'expo-font';
-
-const useFonts = async () =>
-    await Font.loadAsync({
-        proximaNova: require('../../../assets/fonts/ProximaNova.otf'),
-        proximaNovaBold: require('../../../assets/fonts/ProximaNovaBold.otf'),
-        addelleSansBold: require('../../../assets/fonts/AdelleSansEXT-Bold.ttf'),
-    });
-export default useFonts
